5ea551888793f1669f177b0266b55c8cf56d4e3f
dimitr0v4 <coolasallhel@gmail.com>
2025-03-23 20:53:34 +0200

    Update Program.cs

--- 21325s2/Program.cs
+++ 21325s2/Program.cs
@@ -2,21 +2,36 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
-
+            Console.WriteLine("Hello World!");
             Console.WriteLine("enter a number");
             int h = int.Parse(Console.ReadLine());
-            if (h < 5 || h > 5)
+            if (h < 5 || h > 55)
             {
                 Console.WriteLine("the number must be between 5 and 55");
-                h = int.Parse(Console.ReadLine());}
+                h = int.Parse(Console.ReadLine());
+            }
+            Stack<int> st = new Stack<int>();
             
+            int pc = h;
+            for (int i = 0; i < h; i++)
+            { st.Push(ReFib(pc));
+                pc--;
+            }
+
+            for (int i = 0; i < h; i++)
+            { Console.WriteLine(st.Pop()); }
 
 
+        }
 
+        private static int ReFib(int n)
+        { if (n == 0)
+                return 0;
+            else if (n == 1 )
+                return 1;
+            else { return (ReFib(n - 1) + ReFib(n - 2)); }
         }
     }
 }
